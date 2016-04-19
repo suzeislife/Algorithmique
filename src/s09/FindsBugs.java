4531a874c3694206a5e2ec98b6d824c1e52673ef
Jeremy Portelli <jeremyp.1994@hotmail.com>
2016-04-19 10:50:54 +0200

    V1.2

---
+++ src/s09/FindsBugs.java
@@ -0,0 +1,70 @@
+package s09;
+
+public class FindsBugs {
+
+  public static void main(String[] args) {
+    if (!findsBug01())
+      System.out.println("01 NO ERROR");
+    else
+      System.out.println("01 ERROR");
+    if (!findsBug02())
+      System.out.println("02 NO ERROR");
+    else
+      System.out.println("02 ERROR");
+    if (!findsBug03())
+      System.out.println("03 NO ERROR");
+    else
+      System.out.println("03 ERROR");
+    if (!findsBug04())
+      System.out.println("04 NO ERROR");
+    else
+      System.out.println("04 ERROR");
+  }
+
+  static boolean findsBug01() {
+    IntStack s = new IntStack(10);
+    s.push(0);
+    if (s.pop() != 0)
+      return true;
+    if (!s.isEmpty())
+      return true;
+    return false;
+  }
+
+  static boolean findsBug02() {
+    IntStack s = new IntStack(10);
+    s.push(0);
+    s.push(1);
+    if (s.isEmpty())
+      return true;
+    return false;
+  }
+
+  static boolean findsBug03() {
+    IntStack s = new IntStack(10);
+    s.push(0);
+    s.push(5);
+    s.pop();
+    s.push(1);
+    if (s.pop() != 1)
+      return true;
+    if (s.isEmpty())
+      return true;
+    return false;
+  }
+
+  static boolean findsBug04() {
+    IntStack s1 = new IntStack(10);
+    IntStack s2 = new IntStack(10);
+    s1.push(50);
+    s2.push(10);
+    if (s1.top() != 50)
+      return true;
+    if (s1.pop() != 50)
+      return true;
+    if (!s1.isEmpty())
+      return true;
+    return false;
+  }
+
+}
