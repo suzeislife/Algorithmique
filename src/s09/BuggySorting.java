2a4ad13501c2e4e07c133c6505543b1329e5764d
Jeremy Portelli <jeremyp.1994@hotmail.com>
2016-04-19 14:08:05 +0200

    v1.5

--- src/s09/BuggySorting.java
+++ src/s09/BuggySorting.java
@@ -10,7 +10,7 @@ public class BuggySorting {
       t = a[0];
       a[0] = a[1];
       a[1] = t;
-    }
+    } 
     for (i = 0; i < a.length - 1; i++) {
       min = i;
       for (j = i + 1; j < a.length; j++)
