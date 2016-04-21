8e0e90a8d4c0f5c32288df323c8f6bf9c9f0caad
Jeremy Portelli <jeremyp.1994@hotmail.com>
2016-04-21 10:54:00 +0200

    V1.7

--- src/s09/IntStack.java
+++ src/s09/IntStack.java
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-package s09;
-
-public class IntStack {
-  private int buf[];
-  private int top;
-
-  public IntStack() {
-    this(10);
-  }
-
-  public IntStack(int cap) {
-    buf = new int[cap];
-    top = -1;
-  }
-
-  public boolean isEmpty() {
-    return top == -1;
-  }
-
-  public int top() {
-    return buf[top];
-  }
-
-  public int pop() {
-    int a = buf[top];
-    top--;
-    return a;
-  }
-
-  public void push(int x) {
-    checkSize();
-    top++;
-    buf[top] = x;
-  }
-
-  private void checkSize() {
-    if (top == buf.length - 1) {
-      int[] t = new int[2 * buf.length];
-      for (int i = 0; i < buf.length; i++)
-        t[i] = buf[i];
-      buf = t;
-    }
-  }
-=======
 package s09;
 
 public class IntStack {
@@ -104,5 +59,5 @@ public class IntStack {
 			buf = t;
 		}
 	}
->>>>>>> 337115bfa8c2d5108de3b2742a19bdcc81287451
+
 }
